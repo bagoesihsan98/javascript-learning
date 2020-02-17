803751c6a0696a5687ab2ab9e6fa64373a4de3c6
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-02-17 20:04:59 +0700

    Latihan Pengulangan While Pertama

--- first/latihan.js
+++ first/latihan.js
@@ -1,7 +1,16 @@
-var nilai = 1;
+var statusAngkot = true;
+var nomorAngkot = 1;
 
-while(nilai <= 5)
+while(statusAngkot)
 {
-    console.log("Hello World, ini yang ke " + nilai);
-    nilai++;
+    if(nomorAngkot <= 10)
+    {
+        console.log("Angkot No. " + nomorAngkot + " beroperasi dengan baik.");
+        nomorAngkot++;
+        statusAngkot = confirm("Apakah akan menjalankan angkot lagi ?");
+    }else
+    {
+        console.log("Mohon maaf, jumlah armada angkot hanya sebanyak 10 buah.")
+        statusAngkot = false;
+    }
 }
