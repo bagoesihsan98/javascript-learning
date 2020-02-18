479a34f4bf233c53ba521545db7ad9987b2ede2d
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-02-18 20:03:56 +0700

    Latihan Pengulangan ( While dan For )

--- first/latihan.js
+++ first/latihan.js
@@ -1,4 +1,17 @@
-for(var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++)
+var jmlhAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+
+// While
+while(noAngkot <= angkotBeroperasi)
 {
-    console.log("Ini nilai ke " + nilaiAwal);
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+    noAngkot++;
+}
+
+// For
+for(noAngkot; noAngkot <= jmlhAngkot; noAngkot++)
+{
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.");
 }
