c410f29ed8e5a63929a6013c6a50316b79a7d1f0
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-02-19 21:29:36 +0700

    Latihan Pengulangan ( Diamond Center, no edge )

--- first/latihan.js
+++ first/latihan.js
@@ -3,24 +3,24 @@ var segitigaBawah = '';
 
 for(var i = 0; i <= 5; i++)
 {
-    for(var j = 5; j >= i; j--)
+    for(var j = 5; j > i; j--)
+    {
+        segitigaAtas += '  ';
+    }
+
+    for(var j = 0; j <= i; j++)
     {
         segitigaAtas += '* ';
     }
 
     for(var j = 0; j < i; j++)
     {
-        segitigaAtas += '  ';
-    }
-
-    for(var j = 0; j < i; j++)
-    {
-        segitigaAtas += '  ';
-    }
-
-    for(var j = 5; j >= i; j--)
-    {
         segitigaAtas += '* ';
+    }
+
+    for(var j = 5; j > i; j--)
+    {
+        segitigaAtas += '  ';
     }
 
     segitigaAtas += '\n';
@@ -31,24 +31,24 @@ for(var i = 0; i <= 5; i++)
 for(var j = 0; j <= 5; j++)
 {
 
-    for(var k = 0; k <= j; k++)
+    for(var k = 0; k < j; k++)
+    {
+        segitigaBawah += '  ';
+    }
+
+    for(var k = 5; k >= j; k--)
     {
         segitigaBawah += '* ';
     }
 
     for(var k = 5; k > j; k--)
     {
-        segitigaBawah += '  ';
-    }
-
-    for(var k = 5; k > j; k--)
-    {
-        segitigaBawah += '  ';
-    }
-
-    for(var k = 0; k <= j; k++)
-    {
         segitigaBawah += '* ';
+    }
+
+    for(var k = 0; k < j; k++)
+    {
+        segitigaBawah += '  ';
     }
 
     segitigaBawah += '\n';
